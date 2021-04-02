9144accd283177020f5e063d388e55a184a9e95a
Krishna Robertson <keltic19@yahoo.com>
2021-04-02 12:33:31 -0400

    Replaced the mail engine with phpmailer and did successful tests. It's an option to javascript mailer

--- sendMail.php
+++ sendMail.php
@@ -7,10 +7,12 @@ require '.\PHPMailer-master\src\PHPMailer.php';
 require '.\PHPMailer-master\src\SMTP.php';
 //include_once("PHPMailer-master\PHPMailer-master\PHPMailerAutoload.php");
 try{
-$mail = new PHPMailer(true);
+$mail = new PHPMailer(true); //Argument true in constructor enables exceptions
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.office365.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
+//Enable SMTP debugging.
+//$mail->SMTPDebug = 3; 
 $mail->Username = 'krobert4@sgu.edu';                 // SMTP username
 $mail->Password = '_Greyfalcon77@';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
@@ -18,25 +20,25 @@ $mail->Port = 587;
 }
 catch(Exception $e){
 	
-	
+	echo $e;
 	
 }// TCP port to connect to
 if($_SERVER['REQUEST_METHOD']=="POST"){
 	
 	if(isset($_POST['action']) && $_POST['action']=="sendstatement"){
-	$statementid=$_POST['recipient'];
-	$attach=$_POST['attachment'];
-	$bodyTxt=$_POST['body'];
-	$FN=$_POST['fileNumber'];
-	//echo $statementid;
-	//$statementid="krobert4@sgu.edu";
-	sendStatement($statementid,$attach,$bodyTxt,$FN);
-	
-	
-	
-	
-	
-}
+		$statementid=$_POST['recipient'];
+		$attach=$_POST['attachment'];
+		$bodyTxt=$_POST['body'];
+		$FN=$_POST['fileNumber'];
+		//echo $statementid;
+		//$statementid="krobert4@sgu.edu";
+		sendStatement($statementid,$attach,$bodyTxt,$FN);
+	}
+	else if(isset($_POST['action']) && $_POST['action']=="testCredentials"){
+		//retrieve username and password
+		testCredentials($_POST['UN'],$_POST['PW']);
+		
+	}
 	
 }
 
@@ -44,10 +46,24 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
 function sendStatement($con, $att,$bodyText,$fileNum){
 	
 global $mail;
-
+$proceed;
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 try{
+/*//From email address and name
+$mail->From = "from@yourdomain.com";
+$mail->FromName = "Full Name";
 
+//To address and name
+$mail->addAddress("recepient1@example.com", "Recepient Name");
+$mail->addAddress("recepient1@example.com"); //Recipient name is optional
+
+//Address to which recipient will reply
+$mail->addReplyTo("reply@yourdomain.com", "Reply");
+
+//CC and BCC
+$mail->addCC("cc@example.com");
+$mail->addBCC("bcc@example.com");
+*/
 //$mail->confirmReadingTo='loans@forshorelending.com';
 //$mail->addReplyTo('loans@forshorelending.com','reply');
 $mail->setFrom('krobert4@sgu.edu', 'forshorelending');
@@ -57,8 +73,15 @@ $mail->addAddress($con, "krishna"); //To address
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
 //$mail->addBCC('bcc@example.com');
+//decode the base 64 of the pdf file
 $bin = base64_decode($att, true);
-file_put_contents('PDFs/'.$fileNum.'.pdf', $bin);
+//write file to folder
+$proceed = file_put_contents('PDFs/'.$fileNum.'.pdf', $bin);
+if($proceed===false){
+	return "Failed to write file";
+	
+}
+
 $mail->addAttachment('PDFs/'.$fileNum.'.pdf');         // Add attachments
 //$mail->addAttachment($res['location'], 'Your contract');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
@@ -80,17 +103,71 @@ $mail->Body    = $bodyText;
 			*/
 
 	if($mail->send()){
-		echo json_encode("OK");
+		echo "OK";
 	}
 	else{
 		
-		echo json_encode("FAILED");
+		echo "FAILED";
 	}
 }
 catch(Exception $e){
-	echo json_encode($e);
+	echo $e;
 }
 
 }	
+
+
+function testCredentials($un,$pw){
+	
+global $mail;
+$proceed;
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+try{
+
+//$mail->confirmReadingTo='loans@forshorelending.com';
+//$mail->addReplyTo('loans@forshorelending.com','reply');
+$mail->setFrom($un, 'forshorelending');
+$mail->addAddress($un, "krishna"); //To address 
+//$mail->addAddress($useremail, $name);  // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//decode the base 64 of the pdf file
+$mail->Username = $un;                 // SMTP username
+$mail->Password = $pw;    
+//$mail->addAttachment($res['location'], 'Your contract');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Test email.';
+$mail->Body    = "This a test email to verify your credentials. A batch of reports are about to be sent.";
+//$mail->AltBody = '';
+
+/*if(!$mail->send()) {
+	$mail->ErrorInfo;
+    $errors['error']=$mail;
+	$errors['success']=false;
+	echo json_encode($errors);
+	
+} else {
+	$errors['success']=true;
+	echo json_encode($errors);
+				}
+			*/
+
+	if($mail->send()){
+		echo "OK";
+	}
+	else{
+		
+		echo "FAILED";
+	}
+}
+catch(Exception $e){
+	echo " FAILED: ".$e;
+}
+
+}	
+
 
 ?>
